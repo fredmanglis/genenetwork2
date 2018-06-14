db091fdca455623dcb12859ddf8a50fde773431d
Muriithi Frederick Muriuki <fredmanglis@gmail.com>
2018-06-14 14:20:41 +0300

    Fix issues with bytes and strings
    * Since Python3 by default uses utf-8 encoded strings, some items that
      relied on bytestrings and bytearrays led to the system failing.
      Convert such explicitly to Python3's str() type.

--- wqflask/base/trait.py
+++ wqflask/base/trait.py
@@ -398,8 +398,8 @@ def retrieve_trait_info(trait, dataset, get_qtl_info=False):
                 trait.pubmed_link = webqtlConfig.PUBMEDLINK_URL % trait.pubmed_id
 
         if dataset.type == 'ProbeSet' and dataset.group:
-            description_string = unicode(str(trait.description).strip(codecs.BOM_UTF8), 'utf-8')
-            target_string = unicode(str(trait.probe_target_description).strip(codecs.BOM_UTF8), 'utf-8')
+            description_string = str(trait.description).strip(codecs.BOM_UTF8.decode("utf-8")).encode("utf-8")
+            target_string = str(trait.probe_target_description).strip(codecs.BOM_UTF8.decode("utf-8")).encode("utf-8")
 
             if len(description_string) > 1 and description_string != 'None':
                 description_display = description_string
@@ -408,7 +408,7 @@ def retrieve_trait_info(trait, dataset, get_qtl_info=False):
 
             if (len(description_display) > 1 and description_display != 'N/A' and
                     len(target_string) > 1 and target_string != 'None'):
-                description_display = description_display + '; ' + target_string.strip()
+                description_display = str(description_display) + '; ' + str(target_string.strip())
 
             # Save it for the jinja2 template
             trait.description_display = description_display
