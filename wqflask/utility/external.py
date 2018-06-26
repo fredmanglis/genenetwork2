228d2cc91cb2f21917e81defc49121342c203ca8
Muriithi Frederick Muriuki <fredmanglis@gmail.com>
2018-06-26 19:28:19 +0300

    Add utility functions to run external programs
    * Add some generalised utility functions to be used to run external
      programs.

--- wqflask/utility/external.py
+++ wqflask/utility/external.py
@@ -7,3 +7,19 @@ import subprocess
 def shell(command):
     if subprocess.call(command, shell=True) != 0:
         raise Exception("ERROR: failed on "+command)
+
+def shell_with_return(cmd_list):
+    results = subprocess.run(cmd_list, stdout=subprocess.PIPE,
+                             stderr=subprocess.PIPE)
+    the_error = results.stderr.decode("utf-8")
+    if(the_error):
+        raise RuntimeError("shell_with_return failed with {}".format(the_error))
+
+    return results.stdout.decode("utf-8")
+
+def run_with_python2(cmd_args):
+    from . import tools
+    py_list = ["env", "PYTHONPATH={}".format(tools.PYTHON2_PATH),
+                tools.PYTHON2_PROFILE+"/bin/python", "-m"]
+    return shell_with_return(py_list + cmd_args)
+    
